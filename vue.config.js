fe88f8f9bede7bed25781ccb233e6e1576777f97
natan1563 <natromafe@gmail.com>
2023-02-13 19:09:52 -0300

    Dotenv configures

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
-require('dotenv').config();
+const Dotenv = require('dotenv-webpack');
 
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
   configureWebpack: {
-    plugins: [new NodePolyfillPlugin()],
+    plugins: [
+      new NodePolyfillPlugin(),
+      new Dotenv()
+    ],
     optimization: {
       splitChunks: {
         chunks: "all",
       },
     },
+    resolve: {
+      fallback: {
+        fs: false
+      }
+    }
   }
 })
