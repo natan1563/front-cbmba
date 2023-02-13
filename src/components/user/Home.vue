bcb5bb0ff4d245882ec5a90f11844168bb2c1382
natan1563 <natromafe@gmail.com>
2023-02-13 20:36:02 -0300

    feature: loading on delete

--- src/components/user/Home.vue
+++ src/components/user/Home.vue
@@ -249,15 +249,18 @@
         </v-dialog>
         <v-dialog v-model="dialogDelete" max-width="550px">
           <v-card>
-            <v-card-title class="text-h5">Tem certeza que deseja excluir esse usuário?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="red" class="white--text" @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
+              <v-card-title class="text-h5">Tem certeza que deseja excluir esse usuário?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                <v-btn color="red" class="white--text" @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
           </v-card>
         </v-dialog>
+        <v-overlay v-if="onRequestDelete">
+          <v-progress-circular indeterminate></v-progress-circular>
+        </v-overlay>
     </template>
 
     <template v-slot:item.created_at="{ item }">
@@ -409,6 +412,7 @@ export default {
       API_URL: process.env.API_URL,
       dataIsLoading: true,
       onRequestLoggout: false,
+      onRequestDelete: false
     }),
 
     watch: {
@@ -477,6 +481,9 @@ export default {
       },
 
       async deleteItemConfirm () {
+        this.closeDelete()
+        this.onRequestDelete = true;
+
         const token = this.getToken();
         const apiUrl = `${this.API_URL}/users/${this.editedItem.id}?_method=DELETE`;
         try {
@@ -487,10 +494,11 @@ export default {
           });
           
           this.initialize()
-          this.closeDelete()
         } catch (error) {
           let errorMessage = 'Falha ao deletar o usuário';
           this.$root.$emit('active-snackbar', errorMessage)
+        } finally {
+          this.onRequestDelete = false;
         }
       },
 
